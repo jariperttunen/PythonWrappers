f915a12da2d43a809511152c44eeab71574ec585
jarip <>
2014-08-22 15:20:43 +0000

    Now output also for number of regions in the Firmament.

--- PythonWrappers/FirmamentWithMask/pythonDemo.py
+++ PythonWrappers/FirmamentWithMask/pythonDemo.py
@@ -3,37 +3,41 @@ from shutil import *
 # To copy the .so file to the present directory. This is done to run this file from wherever required. So the copy command works like #the first parameter is the source from where the .so file needs to be obtained and then the destination path where it needs to be #copied.
 #copy("FirmamentWithMask/build/*/FirmamentWithMask.so",".")
 from FirmamentWithMask import *
-print "Configuring firmament with 10 inclinations, 20 azimuths, 1 zenith and 1200 MJ total radiant intensity"
+print "Configuring firmament with 20 azimuths, 10 inclinations, 1 zenith and 1200 MJ total radiant intensity"
 d = sky.FirmamentWithMask(10,20,1200)   # calling the FirmamentWithMask constructor.
 VarAzi = d.getNoOfAzimuths()           # Gets the no of Azimuths
 print "Result for getNoOfAzimuths = ", VarAzi  # Prints the results of getNoOfAzimuths.
 VarInc =d.getNoOfInclinations()        # Gets the no of inclinations.
 print "Result for getNoOfInclinations = ", VarInc  # Prints the results of getNoOfInclinations.
 varNoOfregion = d.numberOfRegions()   # Description & gets the number of sector.
+print "Result for numberOfRegions()", varNoOfregion
+
 VarDiffPlane =  d.diffusePlaneSensor()   # Description Intensity of diffuse radiation on a horizontal plane
 print "Result for diffusePlaneSensor =", VarDiffPlane   # Prints the diffusePlaneSensor Value.
 VarBallSenSor =d.diffuseBallSensor()   # Description &Intensity of diffuse radiation as recorded by a ball sensor.
 print "Result for diffuseBallSensor =" , VarBallSenSor # Print on the screen the result of diffuseBallSensor.
 
-print "Resizing the firmament to 20 azimuths, 18 inclinations and with 1200 MJ total radiant intensity"
+print "Resizing the firmament to 20 azimuths, 18 inclinations, 1 zenith and with 1200 MJ total radiant intensity"
 d.resize(20,18,1200)                    # resizing the FirmamentWithMask Object.
 VarAzi = d.getNoOfAzimuths()           # Gets the no of Azimuths
 print "Result for getNoOfAzimuths = ", VarAzi  # Prints the results of getNoOfAzimuths.
 VarInc =d.getNoOfInclinations()        # Gets the no of inclinations.
 print "Result for getNoOfInclinations = ", VarInc  # Prints the results of getNoOfInclinations.
 varNoOfregion = d.numberOfRegions()   # Description & gets the number of sector.
+print "Result for numberOfRegions()", varNoOfregion
 VarDiffPlane =  d.diffusePlaneSensor()   # Description Intensity of diffuse radiation on a horizontal plane
 print "Result for diffusePlaneSensor =", VarDiffPlane   # Prints the diffusePlaneSensor Value.
 VarBallSenSor =d.diffuseBallSensor()   # Description &Intensity of diffuse radiation as recorded by a ball sensor.
 print "Result for diffuseBallSensor =" , VarBallSenSor # Print on the screen the result of diffuseBallSensor.
 
-print "Configuring the firmament from the file Firmament.txt" 
+print "Configuring the firmament from the file Firmament.txt with a gap model" 
 d.readAllMasks("Firmament.txt")       # reading the txt file using readAllMasks.
 VarAzi = d.getNoOfAzimuths()           # Gets the no of Azimuths
 print "Result for getNoOfAzimuths = ", VarAzi  # Prints the results of getNoOfAzimuths.
 VarInc =d.getNoOfInclinations()        # Gets the no of inclinations.
 print "Result for getNoOfInclinations = ", VarInc  # Prints the results of getNoOfInclinations.
 varNoOfregion = d.numberOfRegions()   # Description & gets the number of sector.
+print "Result for numberOfRegions()", varNoOfregion
 print "Radiant intensity without gap model"
 VarDiffPlane =  d.diffusePlaneSensor()   # Description Intensity of diffuse radiation on a horizontal plane
 print "Result for diffusePlaneSensor =", VarDiffPlane   # Prints the diffusePlaneSensor Value.
